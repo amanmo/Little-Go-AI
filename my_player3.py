19abe5d13a3b22837cea96596b271acc68dad270
Aman Mohandas <aman.mohandas@gmail.com>
2021-10-18 15:49:26 -0700

    Submission 7/4 - Added extra weightage to removal of opponent liberties

--- my_player3.py
+++ my_player3.py
@@ -129,7 +129,7 @@ class LittleGo:
         # groupSize = self.findBiggestGroup(board, self.player)
         # opp_groupSize = self.findBiggestGroup(board, 1 if self.player == 2 else 2)
 
-        return val - opp_val + (0.5 * ((liberties - opp_liberties) / moves)) + self. komi #+ (0.4 * ((groupSize - opp_groupSize) / moves))
+        return val - opp_val + (0.5 * ((liberties - (1.2 * opp_liberties)) / moves)) + self. komi #+ (0.4 * ((groupSize - opp_groupSize) / moves))
 
     def hasLiberties(self, board, row, col):
         'Function to judge whether a point has any liberties left'
